6de9a2fce48f50c2d4cd9b1ab96f1da3d3554a91
Rawle Juglal <rawledjuglal@gmail.com>
2016-07-08 01:00:45 +0000

    lesson completed

--- Exercises/week-5/web-fundamentals/templates/routes/fouls.js
+++ Exercises/week-5/web-fundamentals/templates/routes/fouls.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.use(function(req, res, next){
+  console.log('quack');
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Working with foul');
@@ -10,6 +15,9 @@ router.get('/chicken', genericCallback)
   .post('/chicken', postCallback)
   .put('/chicken', putCallback)
   .delete('/chicken', deleteCallback);
+
+router.get('/duck/:id', duckIDCallback);
+router.get('/duck/:id/:temp', duckTempCallback);
 
 function genericCallback(req, res, next){
   res.send('This is the genericCallback');
@@ -26,4 +34,13 @@ function putCallback(req, res, next){
 function deleteCallback(req, res, next){
   res.send('Do you really want to delete this chicken');
 }
+
+function duckIDCallback(req, res){
+  res.send(req.params.id);
+}
+
+function duckTempCallback(req, res, next){
+  res.send(req.params.id + ' ' + req.params.temp);
+}
+
 module.exports = router;
