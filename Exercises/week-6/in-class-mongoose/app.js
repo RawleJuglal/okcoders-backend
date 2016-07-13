dd95c8eeb31bb34e38849885def02379b0d0beaf
Rawle Juglal <rawledjuglal@gmail.com>
2016-07-13 02:16:44 +0000

    adding tuesday notes

---
+++ Exercises/week-6/in-class-mongoose/app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//                'mongodb://localhost/enron'
+//mongoose.connect(url)
+mongoose.connect('mongodb://rawlejuglal-okcoders-summer-back-end-3459472/enron')
+var db = mongoose.connection
+
+db.on('error', function(){
+  console.log('db error!')  
+});
+
+db.once('open', function(){
+  console.log('mongoose connection success!');
+});
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+//a file to require into routes folder for urls calling /emails
+var emails = require('./routes/emails');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+//this actually routes anyone using /emails in the url call to the routes/emails.js file
+app.use('/emails', emails);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
